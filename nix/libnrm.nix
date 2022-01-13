6014941ac86efe081a998257657c6a9843e19e72
Swann Perarnau <swann@anl.gov>
2022-01-13 16:25:04 -0600

    Update nix/libnrm.nix

--- nix/libnrm.nix
+++ nix/libnrm.nix
@@ -2,7 +2,6 @@
 stdenv.mkDerivation {
   src = fetchGit {
     url = "https://github.com/anlsys/libnrm.git";
-    ref = "nrm_topo";
   };
   name = "libnrm";
   nativeBuildInputs = [ autoreconfHook pkgconfig gfortran ];
